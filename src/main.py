afad87f1563af8f63da9028224de87429d45b6e8
Chung-Yi <gary9192631770@gmail.com>
2018-07-07 04:40:53 +0000

    add function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,24 @@ import threading as thread
 import RPi.GPIO as GPIO
 
 class button:
+  def __init__:
+    
+  def init():
   
+  def exit():
+  
+  def 
+
+def init():
+
+def exit():
+
+
+def :
+
+
+
+
 class motor:
   
 def main():
