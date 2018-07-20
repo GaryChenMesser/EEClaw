b8f12eff2807be988d3d4372af96766dacbd77c7
GaryChenMesser <gary9192631770@gmail.com>
2018-07-20 11:43:51 +0800

    add exercise1*, need debug

--- src/exercise1_add.py
+++ src/exercise1_add.py
@@ -1,1 +1,21 @@
+import RPi.GPIO as GPIO
+import time
 
+def my_callback(channel):
+  if GPIO.input(channel):
+    print("Push down!")
+  else:
+    print("Pull up!")
+
+def main():
+  channel = 12
+  GPIO.setmode(channel, GPIO.BCM)
+  GPIO.setup(channel, GPIO.IN)
+
+  GPIO.add_event_detect(channel, )
+
+  while True:
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
--- src/exercise1_wait.py
+++ src/exercise1_wait.py
@@ -1,1 +1,14 @@
+import RPi.GPIO.as GPIO
+import time
 
+channel = 12
+GPIO.setmode(channel, GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+while True:
+  GPIO.wait_for_edge(channel, GPIO.BOTH)
+
+  if GPIO.input(channel):
+    print("Push down!")
+  else:
+    print("Pull up!")
